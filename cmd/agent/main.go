65a16b027b9cbdfb84403d994d1a9af94ad9f548
Victor Chu <chu@vchu.net>
2023-06-09 09:37:21 +0800

    Refactor agent: move configuration to internal package

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,65 +4,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/caarlos0/env/v8"
-	flag "github.com/spf13/pflag"
-
 	"github.com/freepaddler/yap-metrics/internal/agent"
+	"github.com/freepaddler/yap-metrics/internal/agent/config"
 )
-
-const (
-	defaultPollInterval   = 2
-	defaultReportInterval = 10
-	defaultServerAddress  = "127.0.0.1:8080"
-	defaultHttpTimeout    = 1
-)
-
-// global configuration
-type config struct {
-	PollInterval   uint32        `env:"POLL_INTERVAL"`
-	ReportInterval uint32        `env:"REPORT_INTERVAL"`
-	ServerAddress  string        `env:"ADDRESS"`
-	HttpTimeout    time.Duration `env:"HTTP_TIMEOUT"`
-}
 
 func main() {
-	conf := config{}
-
-	// cmd params
-	flag.StringVarP(
-		&conf.ServerAddress,
-		"serverAddress",
-		"a",
-		defaultServerAddress,
-		"metrics collector server address HOST:PORT",
-	)
-	flag.Uint32VarP(
-		&conf.ReportInterval,
-		"reportInterval",
-		"r",
-		defaultReportInterval,
-		"how often to send metrics to server (in seconds)",
-	)
-	flag.Uint32VarP(
-		&conf.PollInterval,
-		"pollInterval",
-		"p",
-		defaultPollInterval,
-		"how often to collect metrics (in seconds)",
-	)
-	flag.DurationVarP(
-		&conf.HttpTimeout,
-		"httpTimeout",
-		"t",
-		defaultHttpTimeout,
-		"http server response timeout (in seconds)",
-	)
-	flag.Parse()
-
-	// env vars
-	if err := env.Parse(&conf); err != nil {
-		fmt.Println("Error while parsing ENV", err)
-	}
+	conf := config.NewConfig()
 
 	fmt.Printf(`Starting agent...
 		server: %s
---
+++ internal/agent/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/caarlos0/env/v8"
+	flag "github.com/spf13/pflag"
+)
+
+const (
+	defaultPollInterval   = 2
+	defaultReportInterval = 10
+	defaultServerAddress  = "127.0.0.1:8080"
+	defaultHttpTimeout    = 1
+)
+
+// Config implements agent configuration
+type Config struct {
+	PollInterval   uint32        `env:"POLL_INTERVAL"`
+	ReportInterval uint32        `env:"REPORT_INTERVAL"`
+	ServerAddress  string        `env:"ADDRESS"`
+	HttpTimeout    time.Duration `env:"HTTP_TIMEOUT"`
+}
+
+func NewConfig() *Config {
+	var c Config
+	// cmd params
+	flag.StringVarP(
+		&c.ServerAddress,
+		"serverAddress",
+		"a",
+		defaultServerAddress,
+		"metrics collector server address HOST:PORT",
+	)
+	flag.Uint32VarP(
+		&c.ReportInterval,
+		"reportInterval",
+		"r",
+		defaultReportInterval,
+		"how often to send metrics to server (in seconds)",
+	)
+	flag.Uint32VarP(
+		&c.PollInterval,
+		"pollInterval",
+		"p",
+		defaultPollInterval,
+		"how often to collect metrics (in seconds)",
+	)
+	flag.DurationVarP(
+		&c.HttpTimeout,
+		"httpTimeout",
+		"t",
+		defaultHttpTimeout,
+		"http server response timeout (in seconds)",
+	)
+	flag.Parse()
+
+	// env vars
+	if err := env.Parse(&c); err != nil {
+		fmt.Println("Error while parsing ENV", err)
+	}
+
+	return &c
+}
