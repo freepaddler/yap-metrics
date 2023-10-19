8e72bd4de9a44f336a42a9fa94a8ad365ab05720
Victor Chu <chu@vchu.net>
2023-10-19 14:15:06 +0800

    Add profiler middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "net/http/pprof"
+
 	"github.com/freepaddler/yap-metrics/internal/app/server"
 	"github.com/freepaddler/yap-metrics/internal/app/server/config"
 	"github.com/freepaddler/yap-metrics/internal/pkg/logger"
